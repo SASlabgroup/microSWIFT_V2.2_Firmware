2450619eff01cfd2c380f1aec35ccacaa18b63e0
philbush <prbush@uw.edu>
2024-08-05 15:46:59 -0700

    getting project cleaned up to build

--- microSWIFT_V2.2/Core/Components/Drivers/Src/generic_uart_driver.c
+++ microSWIFT_V2.2/Core/Components/Drivers/Src/generic_uart_driver.c
@@ -39,7 +39,7 @@ int32_t generic_uart_read ( generic_uart_driver *driver_ptr, uint8_t *read_buf, 
 int32_t generic_uart_write ( generic_uart_driver *driver_ptr, const uint8_t *write_buf,
                              uint16_t size, uint32_t timeout_ticks )
 {
-  HAL_UART_Transmit_DMA (driver_ptr->uart_handle, read_buf, size);
+  HAL_UART_Transmit_DMA (driver_ptr->uart_handle, write_buf, size);
 
   if ( tx_semaphore_get (driver_ptr->uart_sema, timeout_ticks) != TX_SUCCESS )
   {
