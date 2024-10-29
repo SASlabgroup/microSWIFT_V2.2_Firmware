cbd8f10c2d55c58a6489fbb3f34019a2629bf0d0
philbush <prbush@uw.edu>
2024-10-29 14:55:29 -0700

    getting started on light_sensor.c/.h

--- microSWIFT_V2.2/Core/Components/Src/light_sensor.c
+++ microSWIFT_V2.2/Core/Components/Src/light_sensor.c
@@ -12,12 +12,29 @@
 // @formatter:off
 static Light_Sensor *light_self;
 
+// Struct functions
+static light_return_code_t  _light_sensor_setup_sensor (void);
+static light_return_code_t  _light_sensor_read_all_channels (void);
+static light_return_code_t  _light_sensor_get_measurements (uint16_t *buffer);
+static light_return_code_t  _light_sensor_on (void);
+static light_return_code_t  _light_sensor_off (void);
+
 // Functions neccessary for the AS7341 pins
 static bool             __as7341_wait_on_int (uint32_t timeout_ms);
 static GPIO_PinState    __get_as7341_int_pin_state ( void );
 static GPIO_PinState    __get_as7341_gpio_pin_state ( void );
 static void             __set_as7341_int_pin_state ( GPIO_PinState state );
 static void             __set_as7341_gpio_pin_state ( GPIO_PinState state );
+// I/O functions for the sensor
+static int32_t          _light_sensor_i2c_init ( void );
+static int32_t          _light_sensor_i2c_deinit ( void );
+static int32_t          _light_sensor_i2c_read_blocking ( void *unused_handle, uint16_t unused_bus_address,
+                                                          uint16_t reg_address, uint8_t *read_data,
+                                                          uint16_t data_length );
+static int32_t          _light_sensor_i2c_wriet_blocking ( void *unused_handle, uint16_t unused_bus_address,
+                                                           uint16_t reg_address, uint8_t *write_data,
+                                                           uint16_t data_length );
+static void             _light_sensor_ms_delay ( uint32_t delay );
 
 // @formatter:on
 light_return_code_t light_sensor_init ( Light_Sensor *struct_ptr, I2C_HandleTypeDef *i2c_handle,
@@ -57,6 +74,17 @@ light_return_code_t light_sensor_init ( Light_Sensor *struct_ptr, I2C_HandleType
 
   memset (&(light_self->channel_data[0]), 0, sizeof(light_self->channel_data));
 
+  light_self->current_bank = REG_BANK_UNKNOWN;
+
+  light_self->sensor_gain = GAIN_64X;
+
+  light_self->setup_sensor = _light_sensor_setup_sensor;
+  light_self->read_all_channels = _light_sensor_read_all_channels;
+  light_self->get_measurements = _light_sensor_get_measurements;
+  light_self->on = _light_sensor_on;
+  light_self->off = _light_sensor_off;
+
+  // Get the register bank to a known state
   ret |= as7341_set_register_bank (&light_self->dev_ctx, REG_BANK_80_PLUS);
 
   if ( ret == AS7341_OK )
@@ -65,46 +93,66 @@ light_return_code_t light_sensor_init ( Light_Sensor *struct_ptr, I2C_HandleType
   }
   else
   {
-    light_self->current_bank = REG_BANK_UNKNOWN;
+    return LIGHT_I2C_ERROR;
   }
 
+  // Check the chip ID
   ret |= as7341_get_id (&light_self->dev_ctx, &id);
 
-if (id != AS7341_ID)
+  if ( id != AS7341_ID )
+  {
+    ret = LIGHT_I2C_ERROR;
+  }
 }
+
+static light_return_code_t _light_sensor_setup_sensor ( void );
+static light_return_code_t _light_sensor_read_all_channels ( void );
+static light_return_code_t _light_sensor_get_measurements ( uint16_t *buffer );
+static light_return_code_t _light_sensor_on ( void );
+static light_return_code_t _light_sensor_off ( void );
 
 static bool __as7341_wait_on_int ( uint32_t timeout_ms )
 {
-ULONG timeout = (timeout_ms / TX_TIMER_TICKS_PER_SECOND) + 1;
+  ULONG timeout = (timeout_ms / TX_TIMER_TICKS_PER_SECOND) + 1;
 
-if ( tx_semaphore_get (light_self->int_pin_sema, timeout) != TX_SUCCESS )
-{
-  return false;
-}
+  if ( tx_semaphore_get (light_self->int_pin_sema, timeout) != TX_SUCCESS )
+  {
+    return false;
+  }
 
-return true;
+  return true;
 }
 
 static GPIO_PinState __get_as7341_int_pin_state ( void )
 {
-return HAL_GPIO_ReadPin (light_self->gpio_handle->int_pin.port,
-                         light_self->gpio_handle->int_pin.pin);
+  return HAL_GPIO_ReadPin (light_self->gpio_handle->int_pin.port,
+                           light_self->gpio_handle->int_pin.pin);
 }
 
 static GPIO_PinState __get_as7341_gpio_pin_state ( void )
 {
-return HAL_GPIO_ReadPin (light_self->gpio_handle->gpio_pin.port,
-                         light_self->gpio_handle->gpio_pin.pin);
+  return HAL_GPIO_ReadPin (light_self->gpio_handle->gpio_pin.port,
+                           light_self->gpio_handle->gpio_pin.pin);
 }
 
 void __set_as7341_int_pin_state ( GPIO_PinState state )
 {
-HAL_GPIO_WritePin (light_self->gpio_handle->int_pin.port, light_self->gpio_handle->int_pin.pin,
-                   state);
+  HAL_GPIO_WritePin (light_self->gpio_handle->int_pin.port, light_self->gpio_handle->int_pin.pin,
+                     state);
 }
 
 void __set_as7341_gpio_pin_state ( GPIO_PinState state )
 {
-HAL_GPIO_WritePin (light_self->gpio_handle->gpio_pin.port, light_self->gpio_handle->gpio_pin.pin,
-                   state);
+  HAL_GPIO_WritePin (light_self->gpio_handle->gpio_pin.port, light_self->gpio_handle->gpio_pin.pin,
+                     state);
 }
+
+static int32_t _light_sensor_i2c_init ( void );
+static int32_t _light_sensor_i2c_deinit ( void );
+static int32_t _light_sensor_i2c_read_blocking ( void *unused_handle, uint16_t unused_bus_address,
+                                                 uint16_t reg_address, uint8_t *read_data,
+                                                 uint16_t data_length );
+static int32_t _light_sensor_i2c_wriet_blocking ( void *unused_handle, uint16_t unused_bus_address,
+                                                  uint16_t reg_address, uint8_t *write_data,
+                                                  uint16_t data_length );
+static void _light_sensor_ms_delay ( uint32_t delay );
