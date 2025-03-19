3ec633634bb01c42d8b9c2ca66c71e01e34e80aa
philbush <prbush@uw.edu>
2025-03-18 17:51:12 -0700

    Added timestamp_1 --> boot time and refactored RTC alarm setting.

--- microSWIFT_V2.2/Core/Src/main.c
+++ microSWIFT_V2.2/Core/Src/main.c
@@ -135,7 +135,7 @@ int main ( void )
   /* USER CODE BEGIN WHILE */
   while ( 1 )
   {
-    HAL_NVIC_SystemReset ();
+    Error_Handler ();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
