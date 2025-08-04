4a67c76b727ede062485d700871e2c28ff23b465
philbush <prbush@uw.edu>
2025-08-04 10:49:25 -0700

    New branch V2.04

--- microSWIFT_V2.2/run_python_programmer.sh
+++ microSWIFT_V2.2/run_python_programmer.sh
@@ -3,17 +3,17 @@
 
 set -x
 
-rm /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/firmware/microSWIFT_V2.2.elf
+rm /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer/firmware/microSWIFT_V2.2.elf
 
 if [ "$1" == "debug" ]; then
-	cp /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT_V2.2/microSWIFT_V2.2/Debug/microSWIFT_V2.2.elf /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/firmware
+	cp /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT_V2.2/microSWIFT_V2.2/Debug/microSWIFT_V2.2.elf /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer/firmware
 
-	cd /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/
+	cd /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer
 	/opt/homebrew/bin/python3.13 microSWIFT_programmer.py --no_firmware_update --debugger_attached
 
 elif [ "$1" == "deploy" ]; then
-	cp /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT_V2.2/microSWIFT_V2.2/Deployment/microSWIFT_V2.2.elf /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/firmware
-	cd /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/
+	cp /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT_V2.2/microSWIFT_V2.2/Deployment/microSWIFT_V2.2.elf /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer/firmware
+	cd /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer
 	/opt/homebrew/bin/python3.13 microSWIFT_programmer.py --no_firmware_update
 
 else
