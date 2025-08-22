7237e59b908100b21c1f97445b0edb2c2252aeab
Laura Lindzey <lindzey@uw.edu>
2025-08-21 22:24:09 -0700

    Change hard-coded paths in run_python_programmer.sh, make into variable.

--- microSWIFT_V2.2/run_python_programmer.sh
+++ microSWIFT_V2.2/run_python_programmer.sh
@@ -3,12 +3,14 @@
 
 set -x
 
-rm /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer/firmware/microSWIFT_V2.2.elf
+USWIFT_DIR="/Users/lindzey/Documents/Thompson"
+
+rm ${USWIFT_DIR}/microSWIFT-programmer/microSWIFT_Programmer/firmware/microSWIFT_V2.2.elf
 
 if [ "$1" == "debug" ] || [ "$1" == "deploy" ]; then
-	cp /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT_V2.2/microSWIFT_V2.2/Debug/microSWIFT_V2.2.elf /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer/firmware
-	cd /Users/philbush/STM32CubeIDE/microSWIFT/microSWIFT-programmer/microSWIFT_Programmer/src
-	/opt/homebrew/bin/python3.13 microSWIFT_programmer.py --no_firmware_update
+	cp ${USWIFT_DIR}/microSWIFT_V2.2/microSWIFT_V2.2/Debug/microSWIFT_V2.2.elf ${USWIFT_DIR}/microSWIFT-programmer/microSWIFT_Programmer/firmware
+	cd ${USWIFT_DIR}/microSWIFT-programmer/microSWIFT_Programmer/src
+	/Users/lindzey/Documents/Thompson/microSWIFT-programmer/.venv/bin/python3 microSWIFT_programmer.py --no_firmware_update
 
 else
 	echo "Arg not caputred."
